a6fd8bb4a330827281ea49e65d29e3c35f6ec35c
towh2301 <huy.buihoang2301@gmail.com>
2025-10-14 11:17:35 +0700

    update

--- toin/settings.py
+++ toin/settings.py
@@ -7,8 +7,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv(
     "SECRET_KEY", "django-insecure-b!vgt!q$z-k9rwxagd4kv_wqn=dai_uqv1!$5y1!iyzjzaix=i"
 )
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "True").lower() == "true"
 
 ALLOWED_HOSTS = [
@@ -18,7 +16,7 @@ ALLOWED_HOSTS = [
     "toin.onrender.com",
 ]
 
-# Security settings for production
+# Security settings
 SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
 X_FRAME_OPTIONS = "DENY"
@@ -26,17 +24,12 @@ SECURE_HSTS_SECONDS = 31536000
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_HSTS_PRELOAD = True
 
-# DIRECTORIES
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-
-# Application definition
 
 INSTALLED_APPS = [
     "django.contrib.admin",
@@ -50,18 +43,17 @@ INSTALLED_APPS = [
     "converter",
 ]
 
-# Add MIME types for WOFF/WOFF2 and other file types
+# Add MIME types
 mimetypes.add_type("application/font-woff", ".woff", True)
 mimetypes.add_type("font/woff2", ".woff2", True)
 mimetypes.add_type("text/css", ".css", True)
 mimetypes.add_type("application/javascript", ".js", True)
 
-# WhiteNoise settings
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",  # Must be after SecurityMiddleware
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.locale.LocaleMiddleware",  # Only ONE instance
+    "django.middleware.locale.LocaleMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -88,7 +80,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "toin.wsgi.application"
 
-# Database
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
@@ -96,40 +87,23 @@ DATABASES = {
     }
 }
 
-# Password validation
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
     },
-    {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
-    },
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
 
-# Internationalization
-LANGUAGE_CODE = "vi"  # default language
+LANGUAGE_CODE = "vi"
 USE_I18N = True
 USE_L10N = True
 TIME_ZONE = "UTC"
 USE_TZ = True
 
-# List of supported languages
-LANGUAGES = [
-    ("en", "English"),
-    ("vi", "Vietnamese"),
-    ("jp", "Japanese"),
-]
-
-# Where Django will store translation files
-LOCALE_PATHS = [
-    BASE_DIR / "locale",
-]
+LANGUAGES = [("en", "English"), ("vi", "Vietnamese"), ("jp", "Japanese")]
+LOCALE_PATHS = [BASE_DIR / "locale"]
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
