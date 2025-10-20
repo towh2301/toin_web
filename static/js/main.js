ceedae0a4eacf7b173d29d4cbf67cb31512d6a46
towh2301 <huy.buihoang2301@gmail.com>
2025-10-20 10:56:44 +0700

    update

--- static/js/main.js
+++ static/js/main.js
@@ -37,18 +37,18 @@
 		});
 	});
 
-	document
-		.querySelectorAll('.navmenu .toggle-dropdown')
-		.forEach((navmenu) => {
-			navmenu.addEventListener('click', function (e) {
-				e.preventDefault();
-				this.parentNode.classList.toggle('active');
-				this.parentNode.nextElementSibling.classList.toggle(
-					'dropdown-active'
-				);
-				e.stopImmediatePropagation();
-			});
-		});
+	// document
+	// 	.querySelectorAll('.navmenu .toggle-dropdown')
+	// 	.forEach((navmenu) => {
+	// 		navmenu.addEventListener('click', function (e) {
+	// 			e.preventDefault();
+	// 			this.parentNode.classList.toggle('active');
+	// 			this.parentNode.nextElementSibling.classList.toggle(
+	// 				'dropdown-active'
+	// 			);
+	// 			e.stopImmediatePropagation();
+	// 		});
+	// 	});
 
 	const preloader = document.querySelector('#preloader');
 	if (preloader) {
